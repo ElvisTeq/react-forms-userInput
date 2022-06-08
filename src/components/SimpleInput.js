06bea74c668a2c87cf5fcceb4cdee475cf16219e
elvis <cenelvis@gmail.com>
2022-06-08 12:55:53 -0400

    Adding Validation Feedback with useState(true/false)

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,18 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  // True/False => toggle err message and "classNames"
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false); // useState(null) => To skip "useState from below"
+  // True/False => determine if the user has done/touched anything before toggling "messages & classNames"
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false); // Only true => if (input.value in form)
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name input is Valid!");
+    }
+  }, [enteredNameIsValid]);
 
   // useState => Will log/store every key input "onChange"
   const nameInputChangeHandler = (event) => {
@@ -11,6 +21,14 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
 
     // useState
     console.log(enteredName);
@@ -23,9 +41,18 @@ const SimpleInput = (props) => {
     setEnteredName("");
   };
 
+  // true (default) => show invalid
+  // false => valid
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  // true/false => toggle "className"
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -34,6 +61,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {nameInputIsInvalid && (
+          <p className="error-text">Please enter a valid name</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
